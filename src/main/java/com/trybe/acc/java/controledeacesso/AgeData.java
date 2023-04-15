0e6517af113f83a33f7fc775d9c1543c20b613b3
LucianoAAP <lucianoalmeidaap@gmail.com>
2023-04-15 01:37:26 -0300

    first commit

---
+++ src/main/java/com/trybe/acc/java/controledeacesso/AgeData.java
@@ -0,0 +1,61 @@
+package com.trybe.acc.java.controledeacesso;
+
+import java.text.DecimalFormat;
+import java.util.ArrayList;
+import java.util.List;
+
+public class AgeData {
+	private List<Integer> ages = new ArrayList<Integer>();
+	private int minors;
+	private int adults;
+	private int elders;
+	private int total;
+	public List<Integer> getAges() {
+		return ages;
+	}
+	public void setAges(List<Integer> ages) {
+		this.ages = ages;
+	}
+	public int getMinors() {
+		return minors;
+	}
+	public void setMinors(int minors) {
+		this.minors = minors;
+	}
+	public int getAdults() {
+		return adults;
+	}
+	public void setAdults(int adults) {
+		this.adults = adults;
+	}
+	public int getElders() {
+		return elders;
+	}
+	public void setElders(int elders) {
+		this.elders = elders;
+	}
+	public int getTotal() {
+		return total;
+	}
+	public void setTotal(int total) {
+		this.total = total;
+	}
+	
+	public void accessEstablishment(int age) {
+		ages.add(age);
+		this.total += 1;
+		if (age < 18) {
+			this.minors += 1;
+		} else if (age >= 18 && age < 50) {
+			this.adults += 1;
+		} else {
+			this.elders += 1;
+		}
+	}
+	
+	public String getPercentage(int numberOfPeople) {
+		DecimalFormat oneDecimalFormat = new DecimalFormat("#.#");
+		double percentage = (double) numberOfPeople / this.total * 100;
+		return oneDecimalFormat.format(percentage) + "%";
+	}
+}
--- src/main/java/com/trybe/acc/java/controledeacesso/Principal.java
+++ src/main/java/com/trybe/acc/java/controledeacesso/Principal.java
@@ -1,11 +1,56 @@
 package com.trybe.acc.java.controledeacesso;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class Principal {
 
-  /**
-   * Método principal.
-   */
   public static void main(String[] args) {
-    // ESCREVA SEU CÓDIGO AQUI
+	  AgeData ageData = new AgeData();
+	  showMenu(ageData);
+  }
+  
+  private static void showMainMenuMessage() {
+	  System.out.println("Entre com o número correspondente à opção desejada:");
+	  System.out.println("1 - Acessar o estabelecimento");
+	  System.out.println("2 - Finalizar sistema e mostrar relatório");
+  }
+  
+  private static void showMenu(AgeData ageData) {
+	  try {		  
+		  Scanner scanner = new Scanner(System.in);
+		  showMainMenuMessage();
+		  int option = scanner.nextInt();
+		  if (option == 1) {
+			  System.out.print("Entre com a sua idade:");
+			  int age = scanner.nextInt();
+			  if (age < 18) {
+				  System.out.println("Pessoa cliente menor de idade, catraca liberada!");
+			  } else if (age >= 18 && age < 50) {
+				  System.out.println("Pessoa adulta, catraca liberada!");
+			  } else {
+				  System.out.println("Pessoa adulta a partir de 50, catraca liberada!");
+			  }
+			  ageData.accessEstablishment(age);
+			  showMenu(ageData);
+		  } else if (option == 2) {
+			  System.out.println("----- Quantidade -----");
+			  System.out.println(String.format("menores: %s", ageData.getMinors()));
+			  System.out.println(String.format("adultas: %s", ageData.getAdults()));
+			  System.out.println(String.format("a partir de 50: %s", ageData.getElders()));
+			  System.out.println("");
+			  System.out.println("----- Percentual -----");
+			  System.out.println(String.format("menores: %s", ageData.getPercentage(ageData.getMinors())));
+			  System.out.println(String.format("adultas: %s", ageData.getPercentage(ageData.getAdults())));
+			  System.out.println(String.format("a partir de 50: %s", ageData.getPercentage(ageData.getElders())));
+			  scanner.close();
+		  } else {
+			  System.out.println("Entre com uma opção válida!");
+			  showMenu(ageData);
+		  }
+	  } catch (InputMismatchException e) {
+		  System.out.println("Entre com uma opção válida!");
+		  showMenu(ageData);
+	}
   }
 }
